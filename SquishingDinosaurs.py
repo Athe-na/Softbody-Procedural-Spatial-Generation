e75599c04f365590e549bcb324ff5e32c8532432
Athe-na <138402228+Athe-na@users.noreply.github.com>
2025-12-14 22:55:05 -0500

    Implemented very basic GUI and relevant functionality (#1)
    * Cleaned up app drawing, split into simWindow and sidePanel
    
    * Some button progress, troubleshooting.
    
    * Created reset and scale buttons/text box
    
    * removed fluff
    
    * Delete Dinosaur.py

--- SquishingDinosaurs.py
+++ SquishingDinosaurs.py
@@ -3,12 +3,20 @@
 import ctypes
 
 import pygame as pg
+import pygame_widgets as pw
+from pygame_widgets.button import Button
+from pygame_widgets.textbox import TextBox
 import io
 import sys
 import math
 import random
 from Dinosaur import Dinosaur
-from Physics import Engine, PointMass, Wall, Constraint, SoftBody, cross
+from Physics import Engine, PointMass, Wall, SoftBody
+
+
+# Initialize global events
+RESET_EVENT = pg.USEREVENT + 1
+SCALE_EVENT = pg.USEREVENT + 2
 
 def main():
 
@@ -24,22 +32,54 @@ def main():
 
     # Initialize pygame
     pg.init()
+    
 
     # Begin by creating gen area (and window size)
     WIDTH = int(sys.argv[1])
     HEIGHT = int(sys.argv[2])
 
-    reset = True
-
-    while reset:
-
+    # Run the sim for the first time, setting the reset flag on its return value 
+    reset = runSim(WIDTH, HEIGHT)
+    while reset: # If the reset flag is on, reset globals for relevant classes and run the sim again.
+        resetSim()
         reset = runSim(WIDTH, HEIGHT)
 
 def runSim(WIDTH, HEIGHT) -> bool:
 
-    # Initialize the window
+    # Initialize the main window
     window = pg.display.set_mode((WIDTH, HEIGHT))
     window.fill((255,255,255))
+
+    # Create the simulation window
+    simWindow = pg.Surface((WIDTH-400, HEIGHT))
+    simWindow.fill((255,255,255))
+
+    # Create the side panel window
+    sidePanel = pg.Surface((400, HEIGHT))
+    sidePanel.fill((217, 186, 209))
+    
+    # Draw the side panel for the first frame
+    drawSidePanel(sidePanel)
+
+    # Place textboxes
+    scaleBox = TextBox(window, WIDTH-380, 400, 100, 50, placeholdertext="Scale", fontsize=150) 
+
+    # Place buttons
+    resetButton = Button(window, WIDTH-150, 100, 100, 50, text="Reset", fontsize=100, margin=20,
+                         onClick=lambda: pg.event.post(pg.event.Event(RESET_EVENT)))
+    scaleX2Button = Button(window, WIDTH-150, 200, 100, 50, text="Scale x2", fontsize=100, margin=20,
+                           onClick=lambda: pg.event.post(pg.event.Event(SCALE_EVENT, scale=2)))
+    scaleXHALFButton = Button(window, WIDTH-150, 300, 100, 50, text="Scale x2", fontsize=100, margin=20,
+                           onClick=lambda: pg.event.post(pg.event.Event(SCALE_EVENT, scale=0.5)))
+    scaleXHALFButton = Button(window, WIDTH-150, 400, 100, 50, text="Apply Scale", fontsize=100, margin=20,
+                           onClick=lambda: pg.event.post(pg.event.Event(SCALE_EVENT, scale=getScaleFromTextBox(scaleBox))))
+
+    # Initialize sim clock and other guts of program
+    clock = pg.time.Clock()
+    running = True
+    dt = 0
+    elapsedFrames = 0 
+    reset = False
 
     # Create a list of SoftBodies
     softBodies: list[SoftBody] = [
@@ -52,16 +92,9 @@ def runSim(WIDTH, HEIGHT) -> bool:
 
     
     # Initialize the engine
-    e = Engine(softBodies, walls, 0.75, 0.5, 2, WIDTH, HEIGHT)
-    drawEngine(e, window)
+    e = Engine(softBodies, walls, 0.75, 0.5, 2, WIDTH-400, HEIGHT)
+    drawEngine(e, simWindow)
     pg.display.update()
-
-    # Initialize sim clock and other guts of program
-    clock = pg.time.Clock()
-    running = True
-    dt = 0
-    elapsedFrames = 0
-    reset = False
 
     # By default, do not pause the program on the first frame.
     firstFramePause = False
@@ -70,39 +103,59 @@ def runSim(WIDTH, HEIGHT) -> bool:
     if str(sys.argv[3]) == "1":
         print("Paused first frame")
         e.update(dt)
-        window.fill((255, 255, 255))
-        drawEngine(e, window)
-        pg.display.update()            
+        simWindow.fill((255, 255, 255))
+        drawEngine(e, simWindow)
+        pg.display.update() 
+
         dt = 60/1000
         firstFramePause = True
 
         while firstFramePause:
             for event in pg.event.get():
+
                 if event.type == pg.QUIT:
                     firstFramePause = False
                     running = False
+
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_SPACE: # This has a known error where dt is calculated as total elapsed time after unpausing from stepping mode
                         firstFramePause = False
                         dt = 60/1000
+
                     if event.key == pg.K_s:
                         print("Stepping forward")
-
                         e.update(dt)
                         elapsedFrames += 1
-                        window.fill((255, 255, 255))
-                        drawEngine(e, window)
-                        pg.display.update()            
+                        
+                        simWindow.fill((255,255,255)) # Fills the sim window to wipe previous frame.
+                        drawEngine(e, simWindow) # Draws the softbodies in the simWindow
+                        drawApp(WIDTH, HEIGHT, window, simWindow, sidePanel) # Combines the simWindow and the sidePanel onto the main window
+                        pg.display.update() # Performs the window update
+            
                         dt = 60/1000
                 
 
     #Run the sim loop
     while running:
         #Event handling
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        events = pg.event.get()
+        for event in events:
+            if event.type == pg.QUIT: # If the event is a quit, stop running the program
                 running = False
-            if event.type == pg.KEYDOWN:
+            
+            if event.type == RESET_EVENT: # If the event is a custom reset event, set flags for reset
+                reset = True
+                running = False
+                print("RESET_EVENT triggered")
+
+            if event.type == SCALE_EVENT: # If the event is a custom scale event, scale according to the input from the event
+                print(type(event.scale))
+                softBodyScale(e, event.scale)
+
+            if event.type == pg.KEYDOWN: # If a key is pressed...
+                if event.key == pg.K_r: # If r is pressed, set flags for reset
+                    reset = True
+                    running = False
                 if event.key == pg.K_SPACE: # If space is pressed, pause
                     paused = True
                     dt = 60/1000
@@ -111,6 +164,7 @@ def runSim(WIDTH, HEIGHT) -> bool:
                             if event.type == pg.QUIT:
                                 paused = False
                                 running = False
+                                reset = False
                             if event.type == pg.KEYDOWN:
                                 if event.key == pg.K_SPACE: # If space is pressed again, unpause
                                     clock.tick(60)
@@ -119,10 +173,15 @@ def runSim(WIDTH, HEIGHT) -> bool:
                                 if event.key == pg.K_s: # If s is pressed, step forward a frame
                                     print("Stepping forward")
                                     e.update(dt)
-                                    window.fill((255, 255, 255))
-                                    drawEngine(e, window)
-                                    pg.display.update()            
+
+                                    # Code for drawing the app. Really needs to be cleaned up.
+                                    simWindow.fill((255,255,255)) # Fills the sim window to wipe previous frame.
+                                    drawEngine(e, simWindow) # Draws the softbodies in the simWindow
+                                    drawApp(WIDTH, HEIGHT, window, simWindow, sidePanel) # Combines the simWindow and the sidePanel onto the main window
+                                    pg.display.update() 
+
                                     dt = 60/1000
+
                                 if event.key == pg.K_r: # If r is pressed, reset the sim
                                     reset = True
                                     paused = False
@@ -133,25 +192,48 @@ def runSim(WIDTH, HEIGHT) -> bool:
             break
         e.update(dt)
         elapsedFrames += 1
-        window.fill((255, 255, 255))
-        drawEngine(e, window)
-        pg.display.update()            
         dt = clock.tick(60)/1000
 
+        # Code for drawing the app. Really needs to be cleaned up.
+        simWindow.fill((255,255,255)) # Fills the sim window to wipe previous frame.
+        drawEngine(e, simWindow) # Draws the softbodies in the simWindow
+
+
+        # Whites out the engine canvas and then draws the whole app (engine + panel) onto the window
+        window.fill((255, 255, 255), rect=(0,0,WIDTH-400, HEIGHT))
+        window.blit(simWindow, (0,0))
+        window.blit(sidePanel, (WIDTH-400, 0))
+        
+        pw.update(events)
+        pg.display.update() 
+        
+
     print("Sim ended.")
-    resetSim()
     return reset
 
 def resetSim():
     PointMass.IDCounter = 0
     Wall.IDCounter = 0
     SoftBody.IDCounter = 0
+    print("Resetting...")
 
-def scaleFunc(elapsed):
-    if elapsed == 60:
-        print("scaling by " + str(5))
-        return 10
-    return 1
+def getScaleFromTextBox(box: TextBox):
+    text = box.getText()
+    try:
+        num = float(text)
+        return num
+    except:
+        print("Invalid scale provided. Please input a number.")
+        return 1
+    
+def softBodyScale(e: Engine, factor: float):
+    e.scaleSoftBodies(factor)
+
+def drawApp(WIDTH, HEIGHT, base: pg.Surface, engineWindow: pg.Surface, sidePanel: pg.Surface):
+    
+    base.fill((255, 255, 255), rect=(0,0,WIDTH-400, HEIGHT))
+    base.blit(engineWindow, (0,0))
+    base.blit(sidePanel, (WIDTH-400, 0))
 
 
 def drawEngine(e: Engine, window):
@@ -168,7 +250,7 @@ def drawEngine(e: Engine, window):
             # 1 is as far apart as possible (RED)
             colorScale = deltaLength/c.distance
             if colorScale > 1: colorScale = 1
-            print(colorScale)
+            #print(colorScale)
             # Draw a line from point0 to point1, colored according to how close they are to violating the constraint
             pg.draw.aaline(window, (int(255*colorScale), int(255*(1-colorScale)), 0), point0.position, point1.position)
         else:
@@ -201,8 +283,13 @@ def drawEngine(e: Engine, window):
         pg.draw.aaline(window, (0,0,0), point0R, point1L)
 
     for p in e.points: # Loop to draw each point
-        print(str(p) + " @ " + str(p.position))
+        #print(str(p) + " @ " + str(p.position))
         pg.draw.circle(window, (0, 0, 0), p.position, p.radius)
+
+def drawSidePanel(window: pg.Surface):
+    pg.font.init()
+    font = pg.font.Font("resources/Exo2-Regular.ttf", 200)
+
 
 if __name__ == "__main__":
     main()
